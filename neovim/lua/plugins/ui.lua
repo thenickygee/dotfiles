67d08f6121b742b4275d0838958f8dfa4e505962
Nick Gmitter <nickgmitter@icloud.com>
2025-10-14 21:01:37 -0600

    Add UI enhancements configuration for Neovim

---
+++ neovim/lua/plugins/ui.lua
@@ -0,0 +1,27 @@
+-- ==============================
+--  UI Enhancements
+-- ==============================
+
+return {
+  -- Which-key (shows keybind hints)
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {},
+  },
+
+  -- Noice (better UI for messages, cmdline, popupmenu)
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {},
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+  },
+}
