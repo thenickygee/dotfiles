0912994b18d9cdc3dbc317febd28c1c0170d02ae
Nick Gmitter <nickgmitter@icloud.com>
2025-10-14 21:02:18 -0600

    Implement general keymaps in keymaps.lua
    Add general keymaps for search, save, quit, and line movement.

---
+++ neovim/lua/config/keymaps.lua
@@ -0,0 +1,18 @@
+-- ==============================
+--  General Keymaps
+-- ==============================
+
+local map = vim.keymap.set
+
+-- General keymaps
+map("n", "<leader>nh", ":nohlsearch<CR>", { desc = "Clear search highlights" })
+map("n", "<leader>w", ":w<CR>", { desc = "Save file" })
+map("n", "<leader>q", ":q<CR>", { desc = "Quit" })
+
+-- Move lines up/down
+map("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
+map("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
+map("i", "<A-j>", "<Esc>:m .+1<CR>==gi", { desc = "Move line down" })
+map("i", "<A-k>", "<Esc>:m .-2<CR>==gi", { desc = "Move line up" })
+map("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
+map("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
