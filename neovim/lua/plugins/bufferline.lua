2891a36b45595a041696602274cbe8656b8a97b3
Nick Gmitter <nickgmitter@icloud.com>
2025-10-17 15:37:38 -0600

    Add bufferline configuration for Neovim

---
+++ neovim/lua/plugins/bufferline.lua
@@ -0,0 +1,32 @@
+-- ==============================
+--  Bufferline
+-- ==============================
+
+return {
+  "akinsho/bufferline.nvim",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("bufferline").setup({
+      options = {
+        diagnostics = "nvim_lsp",
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "File Explorer",
+            text_align = "left",
+            separator = true,
+          },
+        },
+      },
+    })
+
+    -- Buffer navigation keybindings
+    vim.keymap.set("n", "<leader>bn", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer" })
+    vim.keymap.set("n", "<leader>bp", "<cmd>BufferLineCyclePrev<cr>", { desc = "Previous buffer" })
+    vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Close current buffer" })
+    vim.keymap.set("n", "<leader>bD", "<cmd>BufferLineCloseOthers<cr>", { desc = "Close other buffers" })
+  end,
+}
